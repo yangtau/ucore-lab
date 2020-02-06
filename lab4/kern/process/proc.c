b9d0c51ce81c55e6c5f8c00d9312feac0dda21a6
yangtau <yanggtau+github@gmail.com>
2020-02-06 21:50:06 +0800

    check answers

--- lab4/kern/process/proc.c
+++ lab4/kern/process/proc.c
@@ -111,6 +111,8 @@ alloc_proc(void) {
         proc->mm = NULL;
         proc->tf = NULL;
         proc->flags = 0;
+        memset(proc->name, 0, PROC_NAME_LEN);
+        memset(&(proc->context), 0, sizeof(struct context));
     }
     return proc;
 }
@@ -302,7 +304,6 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
 
     // 1. call alloc_proc to allocate a proc_struct
     if ((proc = alloc_proc()) == NULL) goto bad_fork_cleanup_proc;
-    proc->pid = get_pid();
     // 2. call setup_kstack to allocate a kernel stack for child process
     if (setup_kstack(proc) != 0) goto bad_fork_cleanup_proc;
     // 3. call copy_mm to dup OR share mm according clone_flag
@@ -315,9 +316,15 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     // 4. call copy_thread to setup tf & context in proc_struct
     copy_thread(proc, stack, tf); // TODO: esp
     // 5. insert proc_struct into hash_list && proc_list
-    hash_proc(proc);
-    list_add(&proc_list, &proc->list_link);
-    nr_process++;
+    bool intr_flag;
+    local_intr_save(intr_flag); // NOTE: !important
+    {
+        proc->pid = get_pid();
+        hash_proc(proc);
+        list_add(&proc_list, &(proc->list_link));
+        nr_process ++;
+    }
+    local_intr_restore(intr_flag);
     // 6. call wakeup_proc to make the new child process RUNNABLE
     wakeup_proc(proc);
     // 7. set ret vaule using child proc's pid
